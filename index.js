af617d880628465c1c30f7ed8d1ca685633ed696
Dalton Tewanger <daltontewanger@yahoo.com>
2024-01-14 12:32:39 -0800

    Added the newItem function to include the items that can be clicked and removed from the page.

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ newImage('assets/tree.png', 200, 300);
 newImage('assets/pillar.png', 350, 100);
 newImage('assets/crate.png', 150, 200);
 newImage('assets/well.png', 500, 425);
+
+function newItem(source, left, bottom){
+    let item = newImage(source, left, bottom)
+
+    item.addEventListener('dblclick', () => {
+        item.remove()
+    })
+}
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
